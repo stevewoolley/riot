50220e1b6d8c4b88a05602fd0a5de9afd875d17c
Steve Woolley <steve.woolley@me.com>
2017-01-06 22:06:35 -0500

    fix metrics for metric

--- app.py
+++ app.py
@@ -6,10 +6,17 @@ from chalice import NotFoundError
 from boto3.dynamodb.conditions import Key, Attr
 
 REGION = 'us-east-1'
+ITEMS = 'Items'
+SENSORS_TABLE = 'sensors'
+SENSORS_PARTITION_KEY = 'source'
+SENSORS_SORT_KEY = 'timestamp'
+SENSORS_PAYLOAD = 'payload'
+PAYLOAD_PREFIX = 'payload.state.reported.{}'
 
 app = Chalice(app_name='riot')
 app.debug = True
-ddb = boto3.resource('dynamodb', region_name=REGION)
+
+DDB = boto3.resource('dynamodb', region_name=REGION)
 
 
 def dict_or(d, k1, k2):
@@ -22,8 +29,11 @@ def dict_or(d, k1, k2):
 
 
 def ddb_query(table, response_key, partition_key, partition_value, sort_key=None, params=None, attribute=None):
-    """DynamoDb query using partition key, sort key, and attributes plus gt, gte, lt, lte, and between on sort_key"""
-    p_key = Key(partition_key).eq(partition_value) # query must be partitioned
+    """
+        DynamoDb query using partition key, sort key, and attributes.
+        Also gt, gte, lt, lte, between, begins_with on sort_key.
+    """
+    p_key = Key(partition_key).eq(partition_value)  # query must be partitioned
     if params is not None and sort_key is not None:
         if ('gt' in params or 'gte' in params) and ('lt' in params or 'lte' in params):
             p_key = p_key & Key(sort_key).between(dict_or(params, 'gt', 'gte'), dict_or(params, 'lt', 'lte'))
@@ -36,6 +46,10 @@ def ddb_query(table, response_key, partition_key, partition_value, sort_key=None
                 p_key = p_key & Key(sort_key).lt(params['lt'])
             elif 'lte' in params:
                 p_key = p_key & Key(sort_key).lte(params['lte'])
+            elif 'eq' in params:
+                p_key = p_key & Key(sort_key).eq(params['eq'])
+            elif 'bw' in params:
+                p_key = p_key & Key(sort_key).begins_with(params['bw'])
     if attribute is not None:
         if sort_key is None:
             ex = {"#pkey": partition_key}
@@ -49,7 +63,7 @@ def ddb_query(table, response_key, partition_key, partition_value, sort_key=None
             x.append("#n{}".format(idx))
         response = table.query(
             KeyConditionExpression=p_key,
-            FilterExpression=Attr(attribute).exists(),
+            FilterExpression=Attr(attribute).eq(),
             ExpressionAttributeNames=ex,
             ProjectionExpression=pe.format('.'.join(x))
         )
@@ -82,7 +96,7 @@ def get_thing(thing):
     if request.method == 'GET':
         try:
             response = iot_data.get_thing_shadow(thingName=thing)
-            body = response["payload"]
+            body = response[SENSORS_PAYLOAD]
             return json.loads(body.read())
         except ClientError as e:
             raise NotFoundError(thing)
@@ -95,10 +109,9 @@ def introspect():
 
 @app.route('/metrics/{thing}/{metric}', methods=['GET'])
 def get_metrics(thing, metric):
-    request = app.current_request
     params = app.current_request.query_params
-    if request.method == 'GET':
-        try:
-            return ddb_query(ddb.Table('sensors'), 'Items', 'source', thing, 'timestamp', params, metric)
-        except ClientError as e:
-            raise NotFoundError()
+    try:
+        return ddb_query(DDB.Table(SENSORS_TABLE), ITEMS, SENSORS_PARTITION_KEY, thing, SENSORS_SORT_KEY, params,
+                         PAYLOAD_PREFIX.format(metric))
+    except ClientError as e:
+        raise NotFoundError()
