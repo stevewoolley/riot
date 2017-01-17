acf497c8bd1ced8f690388ec4023dcc9e70b8f51
Steve Woolley <steve.woolley@me.com>
2017-01-17 16:35:37 -0500

    setup jinja2 publishing

--- gensite.py
+++ gensite.py
@@ -4,17 +4,18 @@ import jinja2
 import argparse
 import os
 import json
+import glob
 
 if __name__ == "__main__":
     # parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("templates", nargs='+', help="templates to parse")
+    parser.add_argument("-t", "--templates", help="templates directory", default='templates')
     parser.add_argument("-x", "--extension", help="file extension to export", required=True)
     parser.add_argument("-d", "--output_dir", help="directory to export", default='www')
     parser.add_argument("-j", "--context", help="json context file")
     args = parser.parse_args()
 
-    TEMPLATE_LOADER = jinja2.FileSystemLoader(searchpath="templates")
+    TEMPLATE_LOADER = jinja2.FileSystemLoader(searchpath=args.templates)
     TEMPLATE_ENV = jinja2.Environment(loader=TEMPLATE_LOADER)
     context = None
 
@@ -22,11 +23,14 @@ if __name__ == "__main__":
         with open(args.context) as data_file:
             context = json.load(data_file)
 
-    for t in args.templates:
-        filename, file_extension = os.path.splitext(t)
-        template = TEMPLATE_ENV.get_template(t)
+    for t in glob.glob("{}/*.html.jinja2".format(args.templates)):
+        orig_filename, orig_file_extension = os.path.splitext(t)
+        filename, file_extension = os.path.splitext(orig_filename)
+        orig_basename = os.path.basename(t)
+        export_filename = "{}{}".format(os.path.basename(filename), file_extension)
+        template = TEMPLATE_ENV.get_template(orig_basename)
         if context is not None:
-            template.stream(context).dump("{}/{}.{}".format(args.output_dir, filename, args.extension))
+            template.stream(context).dump("{}/{}".format(args.output_dir, export_filename))
         else:
-            template.stream().dump("{}.{}".format(filename, args.extension))
-        print "input: {}{} output: {}.{}".format(filename, file_extension, filename, args.extension)
+            template.stream().dump("{}/{}".format(args.output_dir, export_filename))
+        print "input: {}{} output: {}".format(filename, file_extension, export_filename)
