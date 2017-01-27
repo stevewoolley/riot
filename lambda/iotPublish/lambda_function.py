3e8c7b6125b7a6606d87f9655577d3a5b18d752a
Steve Woolley <steve.woolley@me.com>
2017-01-27 16:45:49 -0500

    big refactor

--- lambda/iotPublish/lambda_function.py
+++ lambda/iotPublish/lambda_function.py
@@ -1,9 +1,23 @@
 import boto3
 import json
+from boto3.dynamodb.conditions import Key
 
 REGION = 'us-east-1'
 
 
 def lambda_handler(event, context):
     iot_data = boto3.client('iot-data', region_name=REGION)
-    iot_data.publish(topic='/{}'.format(event['topic'].replace('.', '/')), qos=0, payload=json.dumps(event['request']))
+    if 'topic' in event and 'request' in event:
+        iot_data.publish(topic='{}'.format(event['topic']), qos=0, payload=json.dumps(event['request']))
+    elif 'topic' in event:
+        iot_data.publish(topic='{}'.format(event['topic']), qos=0, payload=json.dumps({}))
+    elif 'id' in event:
+        DDB = boto3.resource('dynamodb', region_name=REGION)
+        table = DDB.Table('actions')
+        record = table.query(KeyConditionExpression=Key('uuid').eq(event['id']))['Items']
+        if len(record) == 1:
+            if 'request' in record[0]:
+                iot_data.publish(topic='{}'.format(record[0]['topic']), qos=0, payload=json.dumps(record[0]['request']))
+            else:
+                iot_data.publish(topic='{}'.format(record[0]['topic']), qos=0, payload=json.dumps({}))
+
