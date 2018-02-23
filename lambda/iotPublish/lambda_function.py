fe559e4709637d9b1010e7403a3451e5d762458f
Steve Woolley <steve.woolley@me.com>
2018-02-23 09:24:47 -0500

    massive cleanup and restyle

--- lambda/iotPublish/lambda_function.py
+++ lambda/iotPublish/lambda_function.py
@@ -1,6 +1,7 @@
 import boto3
 import json
 from boto3.dynamodb.conditions import Key
+import logging
 
 REGION = 'us-east-1'
 TOPIC = 'topic'
@@ -14,7 +15,8 @@ def parameter_parser(evt, key):
         return evt[key]
     elif 'pathParameters' in evt and key in evt['pathParameters']:
         return evt['pathParameters'][key]
-    elif 'queryStringParameters' in evt and key in evt['queryStringParameters']:
+    elif 'queryStringParameters' in evt and evt['queryStringParameters'] is not None and key in evt[
+        'queryStringParameters']:
         return evt['queryStringParameters'][key]
     return None
 
@@ -34,17 +36,36 @@ def response(message, status_code):
 def lambda_handler(event, context):
     iot_data = boto3.client('iot-data', region_name=REGION)
     key = parameter_parser(event, KEY)
-    if key:
+    serial_number = parameter_parser(event, 'serialNumber')
+    click_type = parameter_parser(event, 'clickType')
+    if key or (serial_number and click_type):
         ddb = boto3.resource('dynamodb', region_name=REGION)
         table = ddb.Table('triggers')
-        record = table.query(KeyConditionExpression=Key(KEY).eq(key))['Items']
+        search_key = ''
+        if key:
+            search_key = key
+        else:
+            # format search key for IoT Button
+            search_key = '{}_{}'.format(click_type, serial_number)
+        record = table.query(KeyConditionExpression=Key(KEY).eq(search_key))['Items']
         if len(record) > 0:
             if ACTIONS in record[0]:
                 for act in record[0][ACTIONS]:
                     if TOPIC in act:
                         if REQUEST in act:
                             iot_data.publish(topic='{}'.format(act[TOPIC]), qos=0, payload=json.dumps(act[REQUEST]))
+                            print('published topic: {}'.format(act[TOPIC]))
+                            print('published request: {}'.format(act[REQUEST]))
                         else:
                             iot_data.publish(topic='{}'.format(act[TOPIC]), qos=0, payload=json.dumps({}))
-        return response({'key': key}, 200)
+                            print('published topic: {}'.format(act[TOPIC]))
+                    else:
+                        logging.warning('{} not found in triggers table'.format(TOPIC))
+                return response({'key': key}, 200)
+            else:
+                logging.warning('{} not found in trigger record'.format(ACTIONS))
+        else:
+            logging.warning('{} parameter not found in {}'.format(search_key, event))
 
+    else:
+        logging.warning('{} parameter not found in {}'.format(KEY, event))
